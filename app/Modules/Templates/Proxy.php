c35a8da2d28c23d20cf144eb4cdee062214290cb
maximeb97 <maxime.betemps@epitech.eu>
2018-08-03 23:48:37 +0200

    added proxy module template

---
+++ app/Modules/Templates/Proxy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Module\Templates;
+
+class Proxy extends Module
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Download proxies
+     *
+     * @return void
+     */
+    public function downloadProxies()
+    {
+    }
+}
