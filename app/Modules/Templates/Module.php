82038951f8457abf36d2dc1e68ce20caf94d255f
maximeb97 <maxime.betemps@epitech.eu>
2018-08-04 17:24:19 +0200

    Module methods

--- app/Modules/Templates/Module.php
+++ app/Modules/Templates/Module.php
@@ -4,9 +4,46 @@ namespace App\Module\Templates;
 
 class Module
 {
-    const TYPE_PROXY = "proxy";
-
     public function __construct()
     {
+    }
+
+    /**
+     * Get the type of the module
+     *
+     * @return void
+     */
+    public function getType()
+    {
+        return "module";
+    }
+
+    /**
+     * Get a description of the module
+     *
+     * @return void
+     */
+    public function getDescription()
+    {
+        return "A simple module";
+    }
+
+    /**
+     * Get a list of parameters for the module
+     *
+     * @return void
+     */
+    public function getParameters()
+    {
+        /**
+         * Example:
+         * [
+         *  'parameterName1' => 'integer',
+         *  'parameterName2' => 'string'
+         * ]
+         * 
+         */
+        return [
+        ];
     }
 }
